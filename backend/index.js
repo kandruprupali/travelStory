61841b6c373e2f7ae008e569300cbf1cab9ff95a
RupaliKandrup <kandruprupali@gmail.com>
2025-07-31 10:48:26 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config() 
+
+mongoose.connect(process.env.MONGO_URI).then(
+    ()=>{
+        console.log("Database is connecting");
+    }
+).catch((err)=>{
+     console.log(err)
+})
+
+const app=express()
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000!")
+})
