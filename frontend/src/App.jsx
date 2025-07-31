559df70d52905899adfc8540a4f694c038e42109
RupaliKandrup <kandruprupali@gmail.com>
2025-07-31 09:26:05 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'     //rafce shortcut for boiler plate code 
+
+const App = () => {
+  return (
+    <div className="text-pink-300">
+      App
+    </div>
+  )
+}
+
+export default App
