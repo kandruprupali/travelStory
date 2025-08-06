fb29df0684592bfed585323af7668a8791f3656b
RupaliKandrup <kandruprupali@gmail.com>
2025-08-06 23:17:04 +0530

    created login api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from "bcryptjs"
 import User from "../models/User.js"
 import { errorHandler } from "../utils/error.js"
- 
+import jwt from "jsonwebtoken"
  
  export const register = async(req,res,next) =>{
     const{username,email,password}=req.body
@@ -45,7 +45,26 @@ import { errorHandler } from "../utils/error.js"
 
      try 
      {
-       const validUser=await User.findOne()
+       const validUser=await User.findOne({email})
+
+       if(!validUser)
+       {
+        return next(errorHandler(404,"User not found"))
+       }
+
+       const validPassword=bcryptjs.compareSync(password,validUser.password)
+
+       if(!validPassword)
+       {
+         return next(errorHandler(400,"Wrong credentials"))
+       }
+
+       const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET)
+
+       const{password:pass,...rest}=validUser._doc
+       res.status(200).cookie("access_token",token,{
+        httpOnly:true,
+       }).json(rest)
      }
      catch(error)
      {
