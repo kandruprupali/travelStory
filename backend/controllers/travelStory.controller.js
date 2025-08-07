351aefa116b7ed8b897ca7fddc4146959f13b542
RupaliKandrup <kandruprupali@gmail.com>
2025-08-07 15:00:25 +0530

    add travel story api route

---
+++ backend/controllers/travelStory.controller.js
@@ -0,0 +1,41 @@
+import TravelStory from "../models/TravelStory.js"
+import { errorHandler } from "../utils/error.js"
+
+export const addTravelStory = async(req,res,next) =>
+{
+    const {title,story,visitedLocation,imageUrl,visitedDate} = req.body 
+
+    const userId = req.user.id 
+
+    if(!title || !story || !visitedLocation || !imageUrl || !visitedDate)
+    {
+        return next(errorHandler(400,"All fields are required"))
+    }
+
+    //convert bvisited date from miliseconds to dare object 
+    const parsedVisitedDate = new Date(parseInt(visitedDate))
+
+    try 
+    {
+        const travelStory = new TravelStory({
+            title,
+            story,
+            visitedLocation,
+            userId , 
+            imageUrl,
+            visitedDate : parsedVisitedDate ,
+        })
+
+        await travelStory.save()
+
+        res.status(201).json({
+            story:travelStory,
+            message:"your story is added successfully",
+        })
+    }
+
+    catch(error)
+    {
+        next(error)
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import travelStoryRoutes from "./routes/travelStory.route.js"
 
 dotenv.config() 
 
@@ -14,16 +17,18 @@ mongoose.connect(process.env.MONGO_URI).then(
 })
 
 const app=express()
-
+app.use(cookieParser())
 
 //for allowing json object in request body
 app.use(express.json())
-
+/
 app.listen(3000,()=>{
     console.log("server is running on port 3000!")
 })
 
 app.use("/api/auth",authRoutes)
+app.use("/api/user",userRoutes)
+app.use("/api/travel-story",travelStoryRoutes)
 
 
 app.use((err,req,res,next)=>{
