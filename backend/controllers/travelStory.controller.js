402d556b7132204ca8ef5e921f9688361dc7b21a
RupaliKandrup <kandruprupali@gmail.com>
2025-08-11 20:04:43 +0530

    delete image api route

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -1,5 +1,8 @@
+import { fileURLToPath } from "url"
 import TravelStory from "../models/TravelStory.js"
 import { errorHandler } from "../utils/error.js"
+import path from "path"
+import fs from "fs"
 
 export const addTravelStory = async(req,res,next) =>
 {
@@ -74,4 +77,44 @@ export const imageUpload = async(req,res,next) =>{
     {
         next(error)
     }
+}
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rootDir = path.join(__dirname,"..")
+
+export const deleteImage = async(req,res,next) => {
+   const {imageUrl} = req.query 
+
+   if(!imageUrl)
+   {
+    return next(errorHandler(400,"imageUrl parameter is required"))
+   }
+
+   try 
+   {
+       //extract the filename from imageUrl 
+       const filename = path.basename(imageUrl)
+
+       //delete file path 
+       const filePath = path.join(rootDir,"uploads",filename)
+       console.log(filePath)
+
+       //check if the file exists 
+       if(!fs.existsSync(filePath))
+       {
+        return next(errorHandler(404,"Image not found!"))
+       }
+
+       //delete the file 
+       await fs.promises.unlink(filePath)
+
+       res.status(200).json({message:"Image deleted successfully!"})
+   }
+   catch(error)
+   {
+    next(error)
+   }
 }
--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { addTravelStory, getAllTravelStroy, imageUpload } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, getAllTravelStroy, imageUpload } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 const router=express.Router()
 router.post("/image-upload",upload.single("image") , imageUpload)
 router.post("/add",verifyToken,addTravelStory)
 router.get("/get-all",verifyToken,getAllTravelStroy)
+router.delete("/delete-image" , deleteImage)
 
 export default router
