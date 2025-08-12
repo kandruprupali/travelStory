55f7081fbf80ec506de3415489040907d85b60e4
RupaliKandrup <kandruprupali@gmail.com>
2025-08-12 11:18:20 +0530

    search travel story api

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -232,3 +232,35 @@ export const updateIsFavourite = async(req,res,next) =>{
         next(error)
     }
 }
+
+export const searchTravelStory = async(req,res,next) =>{
+ const {query} = req.query 
+ const userId = req.user.id 
+
+ if(!query)
+ {
+    return next(errorHandler(404,"Query is required!"))
+ }
+
+ try 
+ {
+    const searchResults = await TravelStory.find({
+        userId : userId,
+        $or : [
+            {title:{$regex:query ,$options:"i"}},
+            {story:{$regex:query ,$options:"i"}},
+            {visitedLocation:{$regex:query ,$options:"i"}}
+        ]
+        
+    }).sort({isFavorite :-1})
+
+    res.status(200).json({
+        stories:searchResults,
+    })
+ }
+ catch(error)
+ {
+    next(error)
+ }
+
+}
--- backend/routes/travelStory.route.js
+++ backend/routes/travelStory.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyUser.js"
-import { addTravelStory, deleteImage, deleteTravelStory, editTravelStory, getAllTravelStroy, imageUpload, updateIsFavourite } from "../controllers/travelStory.controller.js"
+import { addTravelStory, deleteImage, deleteTravelStory, editTravelStory, getAllTravelStroy, imageUpload, searchTravelStory, updateIsFavourite } from "../controllers/travelStory.controller.js"
 import upload from "../multer.js"
 
 const router=express.Router()
@@ -11,5 +11,6 @@ router.delete("/delete-image" , deleteImage)
 router.post("/edit-story/:id",verifyToken , editTravelStory)
 router.delete("/delete-story/:id",verifyToken,deleteTravelStory)
 router.put("/update-is-favourite/:id",verifyToken,updateIsFavourite)
+router.get("/search",verifyToken,searchTravelStory)
 
 export default router
