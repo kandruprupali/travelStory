486bd24753978cdcee488fe84cd7551ae82677a7
RupaliKandrup <kandruprupali@gmail.com>
2025-08-22 09:26:19 +0530

    redux persist added

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 
       if (response.data) {
         dispatch(signInSuccess(response.data))
-        navigate("/")
+        // navigate("/")
       } else {
         dispatch(signInFailure("An unexpected error occurred!"))
       }
